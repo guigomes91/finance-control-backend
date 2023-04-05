abd7f7d50abeed812929918eb0edd124ac862a4f
guigomes91 <guilherme.gomes91@outlook.com>
2023-04-04 21:30:14 -0300

    Save controller to Account Plan

--- src/main/java/fctrl/com/br/fincontrol/controller/AccountPlanController.java
+++ src/main/java/fctrl/com/br/fincontrol/controller/AccountPlanController.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import fctrl.com.br.fincontrol.model.AccountPlan;
 import fctrl.com.br.fincontrol.repository.AccountPlanRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -32,6 +37,19 @@ public class AccountPlanController {
     @PostMapping(produces = "application/json")
     public AccountPlan save(@RequestBody AccountPlan accountPlan) {
         return repository.save(accountPlan);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AccountPlan> update(@PathVariable @NotNull UUID id, 
+        @RequestBody @Valid AccountPlan accountPlan) {
+        return repository.findById(id)
+            .map(recordFound -> {
+                recordFound.setName(accountPlan.getName());
+                recordFound.setType(accountPlan.getType());
+                AccountPlan updated = repository.save(recordFound);
+                return ResponseEntity.ok().body(updated);
+            })
+            .orElse(ResponseEntity.notFound().build());
     }
 
     @ResponseStatus(code = HttpStatus.OK)
